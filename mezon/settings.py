554b70997581b9f15cebe9ffa918a900972fe170
Muhammadrasul <muhammadrasulaxmedov666@gmail.com>
2026-01-16 18:06:25 +0500

    Оптимизация под телефон

--- mezon/settings.py
+++ mezon/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9rdl^4-y*lacl=ax0a3_7_@q!^4eniu$!kkmx9azv!xp#wm97d"
 
 DEBUG = True
-ALLOWED_HOSTS = ["mezon-market.uz", "www.mezon-market.uz", "45.92.173.192", "127.0.0.1"]
+ALLOWED_HOSTS = ["mezon-market.uz", "www.mezon-market.uz", "45.92.173.192", "127.0.0.1", "dolores-readorning-winter.ngrok-free.dev"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.ngrok-free.dev",
